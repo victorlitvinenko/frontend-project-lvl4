5a3ff0b944c9b9912bd4f5bf8447d568882ab621
Victor Litvinenko <victor@inetlog.ru>
2020-01-20 13:12:13 +0300

    fix (server)

--- server/bin/slack.js
+++ server/bin/slack.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
 /* eslint-disable no-console */
 
-import getApp from '../index.js';
+import getApp from '..';
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const app = getApp();
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server has been started on ${port}`);
