564ff7041674c0f9e10c79c716f86dc8e1d8a3ca
Victor Litvinenko <victor@inetlog.ru>
2020-02-08 17:19:06 +0300

    chore (ChannelsList.jsx) use hooks for the context

--- src/UserContext/index.js
+++ src/UserContext/index.js
@@ -1,4 +1,5 @@
 import { createContext } from 'react';
 
 const UserContext = createContext();
+
 export default UserContext;
--- src/components/ChannelsList.jsx
+++ src/components/ChannelsList.jsx
@@ -1,28 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import UserContext from '../UserContext';
 
-class ChannelsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const ChannelsList = (props) => {
+  const { channels } = props;
+  const userName = useContext(UserContext);
 
-  render() {
-    const { channels } = this.props;
-
-    return (
-      <div className="list-group">
-        {
+  return (
+    <div className="list-group">
+      {
           channels.map(({ id, name }) => (
             <a href={`#${id}`} className="list-group-item list-group-item-action" key={id}>{name}</a>
           ))
         }
-        {this.context}
-      </div>
-    );
-  }
-}
-
-ChannelsList.contextType = UserContext;
+      {userName}
+    </div>
+  );
+};
 
 export default ChannelsList;
